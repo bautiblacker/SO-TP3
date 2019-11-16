77a2fe2a33e6c3f6bf490a3fe92665f1a25bfd74
EugeDamm <euge.damm@gmail.com>
2019-11-15 21:32:27 -0300

    finished game

--- client.c
+++ client.c
@@ -13,9 +13,6 @@
 
 #define PORT 0x5B7F
 
-// argv[0] filename
-// argv[1] server_ipaddress
-// argv[2] port number
 int main(int argc, char const *argv[]) 
 {                                     
     struct sockaddr_in address;
@@ -28,7 +25,7 @@ int main(int argc, char const *argv[])
 
     memset(&serv_addr, '0', sizeof(serv_addr));
 
-    serv_addr.sin_family = AF_INET;  // Dominio, IPv4
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
@@ -36,10 +33,13 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    if (connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Cannot connect to server\n");
+    if(connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection error\n");
         exit(1);
     }
+    
+
+    printf("connection check\n");
 
     char * answer = "entendido\n";
     char * answer2 = "#0854780*\n";
@@ -47,6 +47,11 @@ int main(int argc, char const *argv[])
     char * answer4 = "pistolero\n";
     char * answer5 = "easter_egg\n";
     char * answer6 = ".whatAmI\n";
+    char * answer7 = "indeterminado\n";
+    char * answer8 = "la gioconda\n";
+    char * answer9 = "this is awesome\n";
+    char * answer10 = "abalastro\n";
+    char * answer11 = "gdb es la hostia\n";
 
     sleep(1);
     send(socketfd, answer, strlen(answer), 0);
@@ -60,37 +65,16 @@ int main(int argc, char const *argv[])
     send(socketfd, answer5, strlen(answer5), 0);
     sleep(1);
     send(socketfd, answer6, strlen(answer6), 0);
-    int fd[2];
-    char * buffer = malloc(100);
-    pipe(fd);
-    close(0);
-    dup2(fd[0], 0);
-    read(fd[0], buffer, 100);
-    sleep(2);
+    sleep(1);
+    send(socketfd, answer7, strlen(answer7), 0);
+    sleep(1);
+    send(socketfd, answer8, strlen(answer8), 0);
+    sleep(1);
+    send(socketfd, answer9, strlen(answer9), 0);
+    sleep(1);
+    send(socketfd, answer10, strlen(answer10), 0);
+    sleep(1);
+    send(socketfd, answer11, strlen(answer11), 0);
+    sleep(1);
     return 0;
 }
-
-/*
-    while(1){
-        i=0;
-
-        while((c = getchar()) != '\n'){
-            msg[i] = c;
-            i++;
-        }
-        msg[i] = '\n';
-        write(sock , msg , strlen(msg));
-	
-        bzero(msg,SIZE);
-
-    }
-*/
-
-
-/*
-    SERVER SIDE
-    int newsockfd = accept(int sockfd, (struct sockaddr*)&addr, &addrlen);
-    +-> toda comunicacion sale
-        de este fd    
-
-*/
