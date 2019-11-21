772f7e4c557ed7e10f59612b8a66888fa4417594
bautiblacker <bblacker@itba.edu.ar>
2019-11-21 16:03:19 -0300

    now it waits for user to solve puzzles

--- serverGame.c
+++ serverGame.c
@@ -50,7 +50,7 @@ static char * character = "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)";
 char * filterString(char * string) {
     char * filtered = malloc(strlen(string));
     int i, length = 0;
-    for(i = 0 ; i < strlen(string) ; i++) {
+    for(i = 0; i < strlen(string); i++) {
         if(!isspace(string[i])) {
             if(string[i] >= 'A' && string[i] <= 'Z') {
                 filtered[length++] = tolower(string[i]);
@@ -62,31 +62,31 @@ char * filterString(char * string) {
     return filtered;
 }
 
-int isAnswerCorrect(int challenge, char * answer) {
+int isCorrectAnswer(int challenge, char * answer) {
     char * filteredAnswer = filterString(answer);
     switch (challenge) {
         case 1:
-            return strcmp("estovaleunpuntomas\n", filteredAnswer);
+            return strcmp("estovaleunpuntomas", filteredAnswer);
         case 2:
-            return strcmp("#0854780*\n", filteredAnswer);
+            return strcmp("#0854780*", filteredAnswer);
         case 3:
-            return strcmp("holaariel\n", filteredAnswer);
+            return strcmp("holaariel", filteredAnswer);
         case 4:
-            return strcmp("10\n", filteredAnswer);
+            return strcmp("10", filteredAnswer);
         case 5:
-            return strcmp("easter_egg\n", filteredAnswer);
+            return strcmp("easter_egg", filteredAnswer);
         case 6:
-            return strcmp(".whatAmI\n", filteredAnswer);
+            return strcmp(".whatAmI", filteredAnswer);
         case 7:
-            return strcmp("indeterminada\n", filteredAnswer);
+            return strcmp("indeterminada", filteredAnswer);
         case 8:
-            return strcmp("pi\n", filteredAnswer);
+            return strcmp("pi", filteredAnswer);
         case 9:
-            return strcmp("hellothere\n", filteredAnswer);
+            return strcmp("hellothere", filteredAnswer);
         case 10:
-            return strcmp("unacertijo\n", filteredAnswer);
+            return strcmp("unacertijo", filteredAnswer);
         case 11:
-            return strcmp("gdbinitafull\n", filteredAnswer);
+            return strcmp("gdbinitafull", filteredAnswer);
         default:
             break;
     }
@@ -117,7 +117,7 @@ void understood(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(1, answer)) {
+        if(isCorrectAnswer(1, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
@@ -138,14 +138,13 @@ void numberPad(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(2, answer)) {
+        if(isCorrectAnswer(2, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -159,14 +158,13 @@ void morse(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(3, answer)) {
+        if(isCorrectAnswer(3, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -181,14 +179,13 @@ void EBADF(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(4, answer)) {
+        if(isCorrectAnswer(4, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -202,14 +199,13 @@ void easterEgg(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(5, answer)) {
+        if(isCorrectAnswer(5, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -223,14 +219,13 @@ void whatAmI(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(6, answer)) {
+        if(isCorrectAnswer(6, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -256,14 +251,13 @@ void indetermined(int socket) {
 
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(7, answer)) {
+        if(isCorrectAnswer(7, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -277,14 +271,13 @@ void pi(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(8, answer)) {
+        if(isCorrectAnswer(8, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -298,14 +291,13 @@ void helloThere(int socket) {
         bzero(answer, sizeof(answer));
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(9, answer)) {
+        if(isCorrectAnswer(9, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
@@ -330,7 +322,7 @@ void quine(int socket) {
             bzero(answer, sizeof(answer));
             recv(socket, answer, 1024, 0);
 
-            if(isAnswerCorrect(10, answer)) {
+            if(isCorrectAnswer(10, answer) == 0) {
                 printf("%s%s\n", correctAnswer, answer);
                 sleep(1);
                 return;
@@ -359,14 +351,13 @@ void gdbme(int socket) {
         
         recv(socket, answer, 1024, 0);
 
-        if(isAnswerCorrect(9, answer)) {
+        if(isCorrectAnswer(9, answer) == 0) {
             printf("%s%s\n", correctAnswer, answer);
             sleep(1);
             return;
         } else {
             printf("%s%s\n", incorrectAnswer, answer);
             sleep(1);
-            system("clear");
         }
     }
 }
